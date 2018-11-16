338fc9e21705c002e921a371c1ad8548ddadf7ae
Aleksandr Borodulin <aleksanderborodoolin@mail.ru>
2018-11-16 13:11:27 +0300

    Add simple gradient

--- ImageGradient/Classes/GradientShader.metal
+++ ImageGradient/Classes/GradientShader.metal
@@ -8,11 +8,21 @@
 #include <metal_stdlib>
 using namespace metal;
 
-vertex float4 gradientVertex(const device packed_float3* vertex_array [[ buffer(0) ]],
+struct Vertex
+{
+    float4 position [[position]];
+    half4 color;
+};
+
+vertex Vertex gradientVertex(const device packed_float3* vertex_array [[ buffer(0) ]],
                              unsigned int vid [[ vertex_id ]]) {
-    return float4(vertex_array[vid], 1.0);
+    Vertex vertexOut;
+    vertexOut.position = float4(vertex_array[vid], 1.0);
+    float gradient = (vertexOut.position.y + 1) * 0.5;
+    vertexOut.color = half4(0.0, 1.0, 0.0, gradient);
+    return vertexOut;
 }
 
-fragment half4 gradientFragment() {
-    return half4(1.0, 0.5, 0.5, 1);
+fragment half4 gradientFragment(Vertex inVertex [[stage_in]]) {
+    return inVertex.color;
 }
--- ImageGradient/Classes/IGView.swift
+++ ImageGradient/Classes/IGView.swift
@@ -42,7 +42,7 @@ class IGView: UIView {
     func initialize() {
         device = MTLCreateSystemDefaultDevice()
         commandQueue = device.makeCommandQueue()
-        self.backgroundColor = UIColor.blue
+        //self.backgroundColor = UIColor.blue
         
         initMetalLayer()
         initData()
@@ -63,6 +63,7 @@ class IGView: UIView {
         metalLayer.pixelFormat = pixelFormat
         metalLayer.framebufferOnly = true
         metalLayer.frame = self.layer.frame
+        metalLayer.backgroundColor = UIColor.clear.cgColor
         self.layer.addSublayer(metalLayer)
     }
     
@@ -81,7 +82,9 @@ class IGView: UIView {
     }
     
     func initPipeline() throws {
-        guard let bundle = Bundle(identifier: "org.cocoapods.ImageGradient"), let path = bundle.path(forResource: "default", ofType: "metallib") else { return }
+        guard let bundle = Bundle(identifier: "org.cocoapods.ImageGradient"),
+            let path = bundle.path(forResource: "default", ofType: "metallib")
+            else { return }
         
         let defaultLibrary = try device.makeLibrary(filepath: path)
         let vertexFunction = defaultLibrary.makeFunction(name: "gradientVertex")
@@ -92,6 +95,14 @@ class IGView: UIView {
         pipelineStateDescriptor.fragmentFunction = fragmentFunction
         pipelineStateDescriptor.colorAttachments[0].pixelFormat = pixelFormat
         
+        pipelineStateDescriptor.colorAttachments[0].isBlendingEnabled = true
+        pipelineStateDescriptor.colorAttachments[0].rgbBlendOperation = .add//MTLBlendOperationAdd;
+        pipelineStateDescriptor.colorAttachments[0].alphaBlendOperation = .add//MTLBlendOperationAdd;
+        pipelineStateDescriptor.colorAttachments[0].sourceRGBBlendFactor = .sourceAlpha//MTLBlendFactorSourceAlpha;
+        pipelineStateDescriptor.colorAttachments[0].destinationRGBBlendFactor = .oneMinusSourceAlpha//MTLBlendFactorOneMinusSourceAlpha;
+        pipelineStateDescriptor.colorAttachments[0].sourceAlphaBlendFactor = .sourceAlpha//MTLBlendFactorSourceAlpha;
+        pipelineStateDescriptor.colorAttachments[0].destinationAlphaBlendFactor = .oneMinusSourceAlpha//MTLBlendFactorOneMinusSourceAlpha;
+        
         try pipelineState = device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
     }
     
@@ -101,7 +112,7 @@ class IGView: UIView {
             guard let drawable = metalLayer.nextDrawable() else { return }
             renderPassDescriptor.colorAttachments[0].texture = drawable.texture
             renderPassDescriptor.colorAttachments[0].loadAction = .clear
-            renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+            renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.0)
             
             let commandBuffer = commandQueue.makeCommandBuffer()
             
